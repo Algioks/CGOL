8b8f7c7a9829499d56b28fde5eddf8ba7fcd364f
Algirdas <algirdas.arlauskas1@gmail.com>
2023-08-22 08:35:37 +0300

    provided code

---
+++ initial.py
@@ -0,0 +1,53 @@
+from pprint import pprint
+from random import choice
+from typing import List
+
+# constant
+GENERATIONS = 10
+
+
+def create_map(size: int = 5) -> List[List[bool]]:
+    """Create a 2d map for given size
+    [
+    [1,1,1],
+    [1,1,1],
+    [1,1,1]
+    ]
+
+    """
+    # map_ = []
+    # for _ in range(size):
+    #     row = []
+    #     for _ in range(size):
+    #         row.append(choice([False, True]))
+    #     map_.append(row)
+    #
+    # return map_
+    return [[choice([False, True]) for _ in range(size)] for _ in range(size)]
+
+
+def show_map(map_: List[List[bool]]):
+    # matplotlib library.. google how to do 2d animation.
+    pprint(map_)
+
+
+def get_neighbours(map_, coordinate_row, coordinate_column) -> int:
+    """"""
+    count = 0
+    # HOMEWORK
+    # code....
+    return count
+
+
+def update_map(old_map: List[List[bool]]) -> List[List[bool]]:
+    """Updates given map to next generation"""
+    # HOMEWORK
+    # we iterate over whole map
+    # check number of alive neighbours
+    # decision: live or die
+    # return updated/new map
+
+
+if __name__ == '__main__':
+    current_map = create_map()
+    show_map(current_map)
